ef6f3207ce5397ce3cdfac9c22185f19f89d5760
mubarak <mubarakcali24@gmail.com>
2023-07-15 16:49:44 -0700

    styling app

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -2,44 +2,49 @@ import React from 'react';
 
 function NavTabs({ currentPage, handlePageChange }) {
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <a
-          href="#about"
-          onClick={() => handlePageChange('About')}
-          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-        >
-          About Me
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#portfolio"
-          onClick={() => handlePageChange('Portfolio')}
-          className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
-        >
-          Portfolio
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#contact"
-          onClick={() => handlePageChange('Contact')}
-          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-        >
-          Contact
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#resume"
-          onClick={() => handlePageChange('Resume')}
-          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
-        >
-          Resume
-        </a>
-      </li>
-    </ul>
+    <header>
+      <div className="d-flex align-items-center">
+        <h2>Mubarak24</h2>
+        <ul className="nav nav-tabs ml-4">
+          <li className="nav-item">
+            <a
+              href="#about"
+              onClick={() => handlePageChange('About')}
+              className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+            >
+              About Me
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="#portfolio"
+              onClick={() => handlePageChange('Portfolio')}
+              className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+            >
+              Portfolio
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="#contact"
+              onClick={() => handlePageChange('Contact')}
+              className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+            >
+              Contact
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="#resume"
+              onClick={() => handlePageChange('Resume')}
+              className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+            >
+              Resume
+            </a>
+          </li>
+        </ul>
+      </div>
+    </header>
   );
 }
 
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -4,29 +4,39 @@ import Portfolio from './pages/Portfolio';
 import About from './pages/About';
 import Resume from './pages/Resume';
 import Contact from './pages/Contact';
+import './style.css'
 
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('About');
 
   const renderPage = () => {
-    if (currentPage === 'About') {
-      return <About />;
+    switch (currentPage) {
+      case 'About':
+        return <About />;
+      case 'Portfolio':
+        return <Portfolio />;
+      case 'Contact':
+        return <Contact />;
+      case 'Resume':
+        return <Resume />;
+      default:
+        return <About />;
     }
-    if (currentPage === 'Portfolio') {
-      return <Portfolio />;
-    }
-    if (currentPage === 'Contact') {
-      return <Contact />;
-    }
-    return <Resume />;
   };
 
   const handlePageChange = (page) => setCurrentPage(page);
 
   return (
     <div>
-      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
-      {renderPage()}
+      <header>
+        <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      </header>
+      <main>{renderPage()}</main>
+      <footer>
+        <a href="https://github.com/mubarak24">GitHub</a>
+        <a href="https://linkedin.com">LinkedIn</a>
+        <a href="https://stackoverflow.com">Stack Overflow</a>
+      </footer>
     </div>
   );
 }
