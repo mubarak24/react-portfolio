08acdc7ec2db7f8f8357d9b06663a6cc6f50c33d
mubarak <mubarakcali24@gmail.com>
2023-07-15 17:38:12 -0700

    add to portfolio

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -4,15 +4,15 @@ export default function Portfolio() {
   return (
     <div>
       <h2>Portfolio</h2>
-      <div>
-        <a href="https://thecolorhieu.github.io/Quotes-of-Thrones/" className="portfolio-item">
+      <div className="portfolio-container">
+        <a href="https://thecolorhieu.github.io/Quotes-of-Thrones/" className="portfolio-item picQoT">
           <div>
             <h3>Quotes of Thrones</h3>
             <span>Project 1</span>
           </div>
         </a>
 
-        <a href="https://cinema-finder.herokuapp.com/" className="portfolio-item">
+        <a href="https://cinema-finder.herokuapp.com/" className="portfolio-item picCF">
           <div>
             <h3>Cinema Finder</h3>
             <span>Project 2</span>
